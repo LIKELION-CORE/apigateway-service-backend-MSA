99a13fe3a342d5fee63658599003213b7a0353aa
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2023-09-23 19:03:38 +0900

    :sparkles: apigateway token 검증로직 추가

--- src/main/java/com/example/apigatewayservice/Filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/example/apigatewayservice/Filter/AuthorizationHeaderFilter.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -35,20 +37,33 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            HttpHeaders headers = request.getHeaders();
 
 
             String jwt = exchange.getRequest().getHeaders().get("Authorization").get(0).substring(7);   // 헤더의 토큰 파싱 (Bearer 제거)
+
+            if(exchange.getRequest().getHeaders().get("Authorization")==null){
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"jwt token not valid");
+            }
 
             if (!isJwtValid(jwt)) {
                 throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"jwt token not valid");
             }
 
-            return chain.filter(exchange);
+            ServerHttpRequest requestWithHeader = request.mutate()
+                    .header("username", jwtProvider.getUserIdFromToken(jwt))
+                    .header("Authorization", "Bearer " + jwt)
+                    .build();
+
+            System.out.println("테스트"+jwt);
+//            return chain.filter(exchange);
+            return chain.filter(exchange.mutate().request(requestWithHeader).build());
         };
     }
 
     private boolean isJwtValid(String jwt) {
-        boolean jwtVerify = false;
+        boolean jwtVerify = true;
 
         String subject = null;
 
@@ -62,7 +77,6 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         if (!jwtVerify || subject==null) {
             jwtVerify = false;
         }
-
 
         return jwtVerify;
     }
